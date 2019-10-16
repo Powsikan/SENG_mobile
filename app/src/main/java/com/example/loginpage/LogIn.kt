4edf9bb0d8992b4390e6cbb230f9867f5eed85b4
POWSIKAN <powsi1997@gmail.com>
2019-10-16 18:16:37 +0530

    solid Applied

--- app/src/main/java/com/example/loginpage/LogIn.kt
+++ app/src/main/java/com/example/loginpage/LogIn.kt
@@ -4,21 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import com.example.loginpage.util.ClickInitializer
+import com.example.loginpage.util.OnTextViewClickInitializer
 
-class LogIn : AppCompatActivity() {
+class LogIn : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+        var intent = Intent(this, SignIn::class.java)
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
-        var signInButton=findViewById<TextView>(R.id.sign_btn)
-
-        signInButton.setOnClickListener {
-           var intent=Intent(this,SignIn::class.java)
-            startActivity(intent)
-        }
-
+        var initializer : ClickInitializer = OnTextViewClickInitializer(this)
+        initializer.initialize(R.id.sign_btn)
     }
 }
--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.example.loginpage
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import com.example.loginpage.util.ClickInitializer
+import com.example.loginpage.util.OnButtonClickInitializer
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+        var intent = Intent(this, LogIn::class.java)
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button=findViewById<Button>(R.id.login_btn)
-        button.setOnClickListener {
-            var intent = Intent(this, LogIn::class.java)
-            startActivity(intent)
-        }
+        var initializer : ClickInitializer = OnButtonClickInitializer(this)
+
+        initializer.initialize(R.id.login_btn)
+
     }
 
 
